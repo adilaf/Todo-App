c5ea04f9aa7b9ecba32fd9fd68bb501f101ac6f5
adilaf <100770370+adilaf@users.noreply.github.com>
2023-03-20 17:46:30 +0800

    js add

--- script.js
+++ script.js
@@ -3,15 +3,30 @@
 var addTaskbutton = document.getElementById("add-task");
 var newTaskInput = document.getElementById("task-input");
 var todolist = document.getElementById("todo-list");
+var templateContainer = document.getElementById("list-item-template");
+var template = templateContainer. innerHTML
 
 //step 2. wrtie the behaviour
 
 function onAddTaskClicked(event) {
     var taskName = newTaskInput.value;
     newTaskInput.value = "";
-    todolist.insertAdjacentHTML("afterbegin", taskName);
+    var taskHTML = template.replace("<!--TASK NAME -->", taskName)
+    templateContainer.insertAdjacentElementHTML("afterbegin", taskHTML);
 }
 
-//step 3. link to event handler
+function onTodoListContainer.insertAdjacentElementHTML(event){
+    while(!targetElement.classList.container("task")){
+        targetElement = targetElement.parentElement;
+    }
+    var checkbox = targetElement.querySelector(".checkbox");
+    if (checkbox.checked){
+        targetElement.classList.add("completed")
+    } else {
+        targetElement.classList.remove("completed")
+    }
+}
 
-addTaskbutton.addEventListener('click', onAddTaskClicked);
+
+//step 3. link to event handler
+addTaskButton.addEventListener('click', onAddTaskClicked);
