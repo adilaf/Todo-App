f64d99f8047afeeff1bcea4dc01f238e71368df2
adilaf <100770370+adilaf@users.noreply.github.com>
2023-04-24 21:52:14 +0800

    edited js

--- src/script.js
+++ src/script.js
@@ -1,36 +1,49 @@
-//step 1. find the elements
+/*Get Element by ID*/
+    let addTaskButton = document.getElementById("add-task");
+    let newTaskInput = document.getElementById("task-input");
+    let todoListContainer = document.getElementById("todo-list");
+    
+/* Locate where <script> tag which contains our template  */
+    let templateElement = document.getElementById("list-item-template");
+/* Lets get the template, which is just all the HTML beteen the <script> tag */
+    let template = templateElement.innerHTML;
 
-var addTaskbutton = document.getElementById("add-task");
-var newTaskInput = document.getElementById("task-input");
-var todolist = document.getElementById("todo-list");
-var templateContainer = document.getElementById("list-item-template");
-var template = templateContainer. innerHTML
+/* Step 2. Lets write the function to handle the 'click' event
+ ---------------------------------------------------------------*/
+    function onAddTaskClicked(event) {
+        /* We don't need any information about what triggered the event
+        so we can ignore the 'event' parameter.
+          
+        Now lets get what was typed in the text box on the form*/
+        let taskName = newTaskInput.value;
+        /* Now clear the text box */
+        newTaskInput.value = "";
 
-//step 2. wrtie the behaviour
+        let taskHTML = template.replace("<!-- TASK_NAME -->", taskName);
 
-function onAddTaskClicked(event) {
-    var taskName = newTaskInput.value;
-    newTaskInput.value = "";
-    var taskHTML = template.replace("<!--TASK NAME-->", taskName)
-    templateContainer.insertAdjacentElementHTML("afterbegin", taskHTML);
-}
-
-
-function onTodoListContainer.insertAdjacentElementHTML(event){
-    let targetElement = event.target;
-
-    while(!targetElement.classList.container("task")){
-        targetElement = targetElement.parentElement;
+        todoListContainer.insertAdjacentHTML('beforeend', taskHTML);
     }
-    var checkbox = targetElement.querySelector(".checkbox");
-    if (checkbox.checked){
-        targetElement.classList.add("completed");
-    } else {
-        targetElement.classList.remove("completed");
+    
+    //add an event onClick //
+    function onTodolistClicked(event) {
+        let targetElement = event.target;
+    
+        while (!targetElement.classList.contains("task")) {
+            targetElement = targetElement.parentElement;
+        }
+    
+        let checkbox = targetElement.querySelector(".checkbox");
+    
+        if (checkbox.checked) {
+            targetElement.classList.add("completed");
+        } else {
+            targetElement.classList.remove("completed");
+        }
     }
-}
-
-
-//step 3. link to event handler
-addTaskButton.addEventListener('click', onAddTaskClicked);
-onTodoListContainer.addEventListener('click', onAddTaskClicked);
+    
+    
+    /* Step 3 make the event trigger our functions
+    -----------------------------------------------*/ 
+    
+    addTaskButton.addEventListener('click', onAddTaskClicked);
+    todoListContainer.addEventListener('click', onTodolistClicked);
